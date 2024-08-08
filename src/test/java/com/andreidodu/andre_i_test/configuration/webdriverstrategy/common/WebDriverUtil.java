ba016076ca2ddcfe1c984f35a69805707977e7cd
Andrei Dodu <andrei.dodu@hotmail.it>
2024-08-08 21:44:01 +0200

    refactor

--- src/test/java/com/andreidodu/andre_i_test/configuration/webdriverstrategy/common/WebDriverUtil.java
+++ src/test/java/com/andreidodu/andre_i_test/configuration/webdriverstrategy/common/WebDriverUtil.java
@@ -4,6 +4,9 @@ import org.openqa.selenium.Proxy;
 
 public class WebDriverUtil {
 
+    public static final String OS_WINDOWS = "windows";
+    public static final String FILE_EXTENSION_EXE = ".exe";
+
     public static Proxy buildProxy() {
         Proxy proxy = new Proxy();
         proxy.setAutodetect(false);
@@ -12,8 +15,8 @@ public class WebDriverUtil {
     }
 
     public static String calculateExtension(String os) {
-        if ("windows".equalsIgnoreCase(os)) {
-            return ".exe";
+        if (OS_WINDOWS.equalsIgnoreCase(os)) {
+            return FILE_EXTENSION_EXE;
         }
         return "";
     }
