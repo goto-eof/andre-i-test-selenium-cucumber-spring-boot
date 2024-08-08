b3472c1852a4f9d75a003ed6233bea65c5ab507f
Andrei Dodu <andrei.dodu@hotmail.it>
2024-08-08 21:03:13 +0200

    refactor

--- src/test/java/com/andreidodu/andre_i_test/configuration/CucumberRunnerTest.java
+++ src/test/java/com/andreidodu/andre_i_test/configuration/CucumberRunnerTest.java
@@ -8,7 +8,7 @@ import org.junit.platform.suite.api.Suite;
 import static io.cucumber.core.options.Constants.*;
 
 @Suite
-@IncludeEngines("cucumber")
+@IncludeEngines(ConfigurationConstants.TEST_ENGINE)
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = ConfigurationConstants.GLUE_PROPERTY_VALUE)
 @ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = ConfigurationConstants.FEATURES_PROPERTY_VALUE)
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = ConfigurationConstants.PLUGIN_PROPERTY_VALUE)
--- src/test/java/com/andreidodu/andre_i_test/constants/ConfigurationConstants.java
+++ src/test/java/com/andreidodu/andre_i_test/constants/ConfigurationConstants.java
@@ -11,4 +11,6 @@ public interface ConfigurationConstants {
     String WEB_DRIVER_GECKO_DRIVER = "webdriver.gecko.driver";
     String WEB_DRIVER_CHROME_DRIVER = "webdriver.chrome.driver";
 
+    String TEST_ENGINE = "cucumber";
+
 }
